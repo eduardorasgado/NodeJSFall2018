fc74b363a3f021ac44c2cc536aa60b59f47bdda9
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-09 04:51:52 -0500

    SimonSaysNoGemu: Initializing the game.

--- javascriptReview/SimonSaysNoGemu/js/logic.js
+++ javascriptReview/SimonSaysNoGemu/js/logic.js
@@ -1,2 +1,44 @@
 // SimonSaysNoGemu logic
-alert('Im ready');
+
+const BTN_EMPEZAR = document.getElementById('btnEmpezar');
+const CELESTE = document.getElementById('celeste');
+const VIOLETA = document.getElementById('violeta');
+const NARANJA = document.getElementById('naranja');
+const VERDE = document.getElementById('verde');
+
+class Juego
+{
+    constructor()
+    {
+        this.inicializar();
+        this.generarSecuencia();
+    }
+
+    inicializar()
+    {
+        BTN_EMPEZAR.classList.add('hide');
+        this.nivel = 1;
+        this.colores = {
+            // llenamos una lista con nuestros colores globales
+            celeste: CELESTE,
+            violeta: VIOLETA,
+            naranja: NARANJA,
+            verde: VERDE
+        };
+    }
+
+    generarSecuencia()
+    {
+        // definimos un array de diez ceros y
+        // los vamos llenando
+        this.secuencia = new Array(10).fill(0).map(n => (
+            // generar numeros entre 0 y 3
+            Math.floor(Math.random() * 4)
+        ));
+    }
+}
+
+function empezarJuego()
+{
+    let juego = new Juego;
+}
