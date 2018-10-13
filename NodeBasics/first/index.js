739883773ea8f33e6539ccbe3cb402274dab3ba3
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-13 06:33:25 -0500

    first nodejs code

---
+++ NodeBasics/first/index.js
@@ -0,0 +1,18 @@
+function a()
+{
+	console.log("IMprimiendo funcion A");
+}
+
+function b()
+{
+	console.log("Imprimiendo la funcion B");
+}
+
+function c()
+{
+	console.log("Imprimiendo funcion C");
+}
+
+a();
+b();
+c();
