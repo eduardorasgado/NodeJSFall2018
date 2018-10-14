54e4681a563e324b5113742ccf780625cc76eacd
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-13 19:03:28 -0500

    Callback functions

---
+++ NodeBasics/callbacks/src/call.js
@@ -0,0 +1,25 @@
+module.exports = {
+    sync: function(nombre)
+    {
+        return `Hola ${nombre}! Bienvenido a Node.js`;
+    },
+
+    withCallback: function(nombre, callback)
+    {
+        setTimeout(() =>
+        {
+            callback(nombre)
+        }, 2000);
+    },
+
+    withPromise: function (nombre)
+    {
+        return new Promise((resolve, reject) =>
+        {
+            setTimeout(() =>
+            {
+                resolve(nombre);
+            }, 2000);
+        });
+    }
+}
