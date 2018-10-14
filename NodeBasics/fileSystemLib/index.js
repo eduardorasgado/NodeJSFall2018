ebd4bae3b989e0d49d5dcf7513f497a0c5140d49
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-10-13 23:29:04 -0500

    File system node js module:p2

--- NodeBasics/fileSystemLib/index.js
+++ NodeBasics/fileSystemLib/index.js
@@ -7,4 +7,38 @@ const fileops = require("./src/fileops");
 *   https://nodejs.org/dist/latest-v8.x/docs/api/fs.html
 * */
 
-fs.readFile("./resources/number.txt", );
+// donde se van aguardar los datos para posteriormente pasarlos a un archivo txt
+let incValues;
+
+function saveNewData(incValues)
+{
+    // sera llamada dentro del callback de fs.readFile
+    // para gurdar la data modificada del archivo txt
+    fs.writeFile("./resources/number(2).txt", incValues.join("\n"), (err, result) =>
+    {
+        if(err) throw err;
+        console.log("Guardado con éxito");
+    })
+}
+
+fs.readFile("./resources/number.txt", "utf8", (err, data) =>
+{
+    // abre el archivo txt en el directorio resources de nuestro OS
+    // en caso de existir un error
+    if(err) throw err;
+    //console.log(typeof(numbers));
+    let myArray = data.split("\n");
+    myArray.map((n, k) => console.log(`number[${k}] is: ${n}`));
+    console.log("\n");
+    // modificando a la funcion
+    // mandando a llamar al modulo descrito en src
+
+    // convertir los strings a numeros
+    myArray.map((n) => Number(n));
+    // incrementar cada uno de los valores del array
+    incValues = fileops.incrementValues(myArray);
+    incValues.map((n, k) => console.log(`number[${k}] is: ${n}`));
+
+    // guardando los datos
+    saveNewData(incValues);
+});
--- NodeBasics/fileSystemLib/src/fileops.js
+++ NodeBasics/fileSystemLib/src/fileops.js
@@ -1,8 +1,13 @@
 module.exports = {
     incrementValues: function(list)
     {
+        // se guarda la funcion que va a ser ocupada por map
         const mapIncrement = val => Number(val) + 1;
+        // se mapea la lista y almismo tiempo se pasa como paramentro
+        // la constante anteriormente descrita
         return list.map(mapIncrement);
+        // asi se veria de manera conjunta:
+        //return list.map(val => Number(val) + 1);
     },
 
     callNames: function(list)
