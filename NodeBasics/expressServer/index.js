f2d4e29a7c63079ad2576a87f76520d69bd99ac9
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-12-07 23:00:15 -0600

    Express: Solving routes do not exist.

--- NodeBasics/expressServer/index.js
+++ NodeBasics/expressServer/index.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 
 // Server ROUTES
+/*
+* NOTE: Express always solve routing by sequential reading, so, * should
+* be at the end or the other routes wont response correctly
+* */
 app.get("/", (request, response) =>
 {
     response.send("[HOME ROUTE] => <a href='/app'>Go App</a>");
@@ -10,6 +14,11 @@ app.get("/", (request, response) =>
 app.get("/app", (request, response) =>
 {
     response.send("[APP ROUTE] => <a href='/'>Go Home</a>");
+});
+app.get("*", (request, response) =>
+{
+    // user tries to access routes do not exist
+    response.send("[ SITE NOT FOUND ] => Why dont you go home? <a href='/'>Go home</a>");
 });
 
 // start server
