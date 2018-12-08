8bd116e1852fbbe5e274f4ce315775874c13ca02
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-12-07 22:50:55 -0600

    Express: Defining routes to our web app.

--- NodeBasics/expressServer/index.js
+++ NodeBasics/expressServer/index.js
@@ -2,6 +2,17 @@ const express = require("express");
 // http service instance
 const app = express();
 
+// Server ROUTES
+app.get("/", (request, response) =>
+{
+    response.send("[HOME ROUTE] => <a href='/app'>Go App</a>");
+});
+app.get("/app", (request, response) =>
+{
+    response.send("[APP ROUTE] => <a href='/'>Go Home</a>");
+});
+
+// start server
 app.listen(3000, () =>
 {
     // server
