23a83704e2d1ca538453de11ac97ec98c69e30ad
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-12-08 03:46:44 -0600

    Express API: index is done.

--- NodeBasics/expressAPI/index.js
+++ NodeBasics/expressAPI/index.js
@@ -6,16 +6,39 @@ const app = express();
 // loading some settings for server enviroment
 const { PORT } = require("./config");
 
+const mongo = require("./database/connect");
+
 require("./routes/api")(app);
 require("./routes/views")(app);
 
-function init()
+async function initDB()
+{
+    const db = await mongo.connect();
+    if(db)
+    {
+        initExpress();
+    }
+}
+
+function initExpress()
 {
     console.log("[EXPRESS IS INITIALIZED]");
     app.listen(PORT, () =>
     {
         console.log("[EXPRESS SERVER IS ONLINE]");
     });
+    // awaiting two signals in case express is closed
+    process.on("SIGINT", closeApp);
+    process.on("SIGTERM", closeApp);
 };
 
-init();
+function closeApp()
+{
+    mongo.disconnect()
+        .then(() =>
+        {
+            process.exit(0);
+        })
+}
+
+initDB();
